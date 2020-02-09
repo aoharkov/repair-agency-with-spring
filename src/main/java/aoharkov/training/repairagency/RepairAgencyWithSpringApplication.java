068a0a6b29ec6853ef35800a1b0b1e6f00e65abd
Artem Oharkov <aoharkov@kse.org.ua>
2020-02-10 01:47:54 +0200

    Initial commit after using Spring Boot

---
+++ src/main/java/aoharkov/training/repairagency/RepairAgencyWithSpringApplication.java
@@ -0,0 +1,13 @@
+package aoharkov.training.repairagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairAgencyWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairAgencyWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aoharkov/training/repairagency/RepairAgencyWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package aoharkov.training.repairagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairAgencyWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
