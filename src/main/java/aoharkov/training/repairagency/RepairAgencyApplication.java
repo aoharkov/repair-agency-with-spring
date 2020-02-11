f75ffea078d13ea4affd2556e370ed3177571560
Artem Oharkov <aoharkov@kse.org.ua>
2020-02-12 01:10:16 +0200

    use shorter name

--- src/main/java/aoharkov/training/repairagency/RepairAgencyApplication.java
+++ src/main/java/aoharkov/training/repairagency/RepairAgencyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RepairAgencyWithSpringApplication {
+public class RepairAgencyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RepairAgencyWithSpringApplication.class, args);
+        SpringApplication.run(RepairAgencyApplication.class, args);
     }
 
 }
