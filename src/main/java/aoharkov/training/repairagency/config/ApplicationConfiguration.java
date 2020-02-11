c97b8a60b8d4fa04661efbccbf27b45fac7dcaa6
Artem Oharkov <aoharkov@kse.org.ua>
2020-02-11 17:05:25 +0200

    add draft version of controller

--- src/main/java/aoharkov/training/repairagency/config/ApplicationConfiguration.java
+++ src/main/java/aoharkov/training/repairagency/config/ApplicationConfiguration.java
@@ -7,8 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class ApplicationConfiguration {
 
     @Bean
     BCryptPasswordEncoder passwordEncoder() {
