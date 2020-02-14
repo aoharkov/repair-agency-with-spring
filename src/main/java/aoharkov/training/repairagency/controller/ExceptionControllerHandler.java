246c3f119e72ddbc2d8a5aa3ad7fa51e065ae6df
Artem Oharkov <aoharkov@kse.org.ua>
2020-02-14 08:51:59 +0200

    add @Log4j2

--- src/main/java/aoharkov/training/repairagency/controller/ExceptionControllerHandler.java
+++ src/main/java/aoharkov/training/repairagency/controller/ExceptionControllerHandler.java
@@ -1,20 +1,19 @@
 package aoharkov.training.repairagency.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Log4j2
 @ControllerAdvice
 public class ExceptionControllerHandler {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionControllerHandler.class);
 
     @ExceptionHandler(Throwable.class)
     public String exception(HttpServletRequest req, Throwable e) {
         StringBuffer requestUrl = req.getRequestURL();
-        LOGGER.error("Request: {} raised {}", requestUrl, e);
+        log.error("Request: {} raised {}", requestUrl, e);
         return "error500.html";
     }
 }
