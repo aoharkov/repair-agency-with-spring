903fd221106f90d30dfaec20abd34dac896ad10f
Artem Oharkov <aoharkov@kse.org.ua>
2020-03-03 21:20:52 +0200

    update database

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,108 +1,218 @@
 INSERT INTO repair_stages (name)
-VALUES ('Waiting for its turn');
+VALUES ('Waiting for delivery to service center');
 
 INSERT INTO repair_stages (name)
-VALUES ('Teardown');
+VALUES ('On delivery to master');
 
 INSERT INTO repair_stages (name)
-VALUES ('Parts Ordering');
+VALUES ('Issue confirmation');
 
 INSERT INTO repair_stages (name)
-VALUES ('Structural Repair');
+VALUES ('Repair');
 
 INSERT INTO repair_stages (name)
-VALUES ('Body Repair');
+VALUES ('Assembly');
 
 INSERT INTO repair_stages (name)
-VALUES ('Refinishing and Painting');
+VALUES ('Final testing');
 
 INSERT INTO repair_stages (name)
-VALUES ('Reassembly');
+VALUES ('On delivery to service centre');
 
 INSERT INTO repair_stages (name)
-VALUES ('Finished');
+VALUES ('Ready to pick up');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Alex', 'Kr.', 'alex@gmail.com', '$2a$10$zsXYMYK3GxuGghmwrAQWVO5wpK2oQpfpc8KKJvypzIhPsBZLK63TK', 'MASTER');
+VALUES ('Alex', 'Kr.', 'alex@gmail.com', '$2a$10$gOIJt9MD21n3srRFc9T1.upEHbZYr2VcNrTlWBlR3GM217wekKSKW', 'MASTER');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Anatolii', 'K.', 'anatolii@gmail.com', '$2a$10$Bcej5u/3CoKDDbsYhAC.B.IAy39xQKU0KY8GrGnlu1mJUUUw9tgve',
+VALUES ('Anatolii', 'K.', 'anatolii@gmail.com', '$2a$10$Y.E9czp4aJpNqZEEFIjIkeoFEeRzU2MaWdKJOdMGC/EqLOUXLR0dO',
         'CLIENT');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Artem', 'Oh.', 'artem@gmail.com', '$2a$10$Z..BQCkXXeuI3PqtvUexWuTO3NU8P13wFqmgLNz737qqGWFZN7B0C', 'ADMIN');
+VALUES ('Artem', 'Oh.', 'artem@gmail.com', '$2a$10$hTLBwMuzWuntQAFmZpS.XehvPmZsZ8AXkrg7p5VUzJAuFOmQPkbFa', 'ADMIN');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Bohdan', 'K.', 'bohdan@gmail.com', '$2a$10$97DFSDWuGbR66gYw5J.jxugMIMsCuECIyZPnp5w6ULCsFw5ung/Cq', 'MASTER');
+VALUES ('Bohdan', 'K.', 'bohdan@gmail.com', '$2a$10$zK8Xg96lSKCQDNHyYrD.4.6ZAvKGVwd5fTeBwt8TtgcvugyJfZaYO', 'MASTER');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Dmytro', 'Pr.', 'dmytro.pr@gmail.com', '$2a$10$KTJuvjXIaXdHhrGHlM41Y.zIqZ2gTi4Tz2szKtjYPeBHvwglOOO0.',
+VALUES ('Dmytro', 'Pr.', 'dmytro.pr@gmail.com', '$2a$10$r5ppYHLqbPQsjXMVo1iamuQwimQFUu2SbpY4WMq9C516zD.Zl6SM2',
         'CLIENT');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Dmytro', 'St.', 'dmytro@gmail.com', '$2a$10$yYE9GRCJrfSzBtpsQdBGkeUWqAgcmjSbzWTtsDHC.xZMdvLaXOUru', 'CLIENT');
+VALUES ('Dmytro', 'St.', 'dmytro@gmail.com', '$2a$10$WgCH8nsshdiZwg67SvKNiu/HZ63g48TeBslqbpFQmIoFPSEj82sxe', 'CLIENT');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Elizabeth', 'V.', 'elizabeth@gmail.com', 'elizabeth@gmail.com', 'MANAGER');
+VALUES ('Elizabeth', 'V.', 'elizabeth@gmail.com', '$2a$10$Bfk6gCvgZ0dPseIV6SVv.OULF3A2ilzGkb4EKk4/lCo78IhGiJRaa',
+        'MANAGER');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Evhenii', 'Shm.', 'evhenii@gmail.com', 'evhenii@gmail.com', 'CLIENT');
+VALUES ('Evhenii', 'Shm.', 'evhenii@gmail.com', '$2a$10$OCQ1bqHTum8uAKHIQNmrb.Zj3DgtquDKFxDG6ICk5Gc3M36o36GLe',
+        'CLIENT');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('George', 'Ch.', 'george@gmail.com', 'george@gmail.com', 'CLIENT');
+VALUES ('George', 'Ch.', 'george@gmail.com', '$2a$10$lDQQZyXJBH/4/jlVSUo5U.9BePVpH6nhTnclP0mSxPpzAezn/29CO', 'CLIENT');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Ihor', 'P.', 'ihor@gmail.com', 'ihor@gmail.com', 'MASTER');
+VALUES ('Ihor', 'P.', 'ihor@gmail.com', '$2a$10$brExH5OxiQeakpXqMEzOHun.1BNa758CqXbIK.M4oNOu6TxBPUYwK', 'MASTER');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Ivan', 'M.', 'ivan@gmail.com', 'ivan@gmail.com', 'CLIENT');
+VALUES ('Ivan', 'M.', 'ivan@gmail.com', '$2a$10$JRW1tUmpw1cf2HP7CyN53OE2V0pNEF6pa4tf0kZUI/TPLpjqLt.tW', 'CLIENT');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Kateryna', 'B.', 'kateryna@gmail.com', 'kateryna@gmail.com', 'MANAGER');
+VALUES ('Kateryna', 'B.', 'kateryna@gmail.com', '$2a$10$Ca8uOLIfIjmPn9WaaPCyvO1WXzcmniXk8qMEoe7hvjPCfgP6bkAqe',
+        'MANAGER');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Nikita', 'K.', 'nikita@gmail.com', 'nikita@gmail.com', 'MASTER');
+VALUES ('Nikita', 'K.', 'nikita@gmail.com', '$2a$10$tkaP3Y2xn.jjAAC56mxtuepkxQkEiRlLpY/KknGIs4oM.CsdUFAYm', 'MASTER');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Olena', 'P.', 'olena@gmail.com', 'olena@gmail.com', 'MANAGER');
+VALUES ('Olena', 'P.', 'olena@gmail.com', '$2a$10$b.4.926FswtpbdNjgey9.uQ35OjvCL5GGjDgD6FRb53/5GdF6jUcG', 'MANAGER');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Olha', 'B.', 'olha@gmail.com', 'olha@gmail.com', 'CLIENT');
+VALUES ('Olha', 'B.', 'olha@gmail.com', '$2a$10$AtqinJgn2uVi2ikXJqOFz.E7J1w3GXwdiWMDhPKVf0b2L66lJa4qu', 'CLIENT');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Stanislav', 'S.', 'stanislav@gmail.com', 'stanislav@gmail.com', 'CLIENT');
+VALUES ('Stanislav', 'S.', 'stanislav@gmail.com', '$2a$10$dlnGitkDszl/pe6dS31zCOuZazgACLQMy8dqzEwM7pBOgNV06Vg0G',
+        'CLIENT');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Viktor', 'Ovs.', 'viktor@gmail.com', 'viktor@gmail.com', 'MASTER');
+VALUES ('Viktor', 'Ovs.', 'viktor@gmail.com', '$2a$10$gyGNIpbGS4xp2807m2FRX.LBUR1vtX.YHcAf2wbQe4KRWWSnkworm', 'MASTER');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Viktoriia', 'Ol.', 'viktoriia@gmail.com', 'viktoriia@gmail.com', 'CLIENT');
+VALUES ('Viktoriia', 'Ol.', 'viktoriia@gmail.com', '$2a$10$jIv.kSGBt1LpjxFEPBnjZuLahj0.oLLhu3.sZAN3k1Tgh2Ba0LqYG',
+        'CLIENT');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Vitalii', 'Iv.', 'vitalii@gmail.com', 'vitalii@gmail.com', 'CLIENT');
+VALUES ('Vitalii', 'Iv.', 'vitalii@gmail.com', '$2a$10$ZWiKBc01HslDhT2kYWLomOEzHAsuL8UeXvosjp6AzgssLOhzx0Ju6',
+        'CLIENT');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Vitalii', 'V.', 'vitalii.v@gmail.com', 'vitalii.v@gmail.com', 'CLIENT');
+VALUES ('Vitalii', 'V.', 'vitalii.v@gmail.com', '$2a$10$WS1OvyCUYO25FSx/GG4.MumduuWCTO2vNziWym9D911zdfpqq4yPO',
+        'CLIENT');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Yurii', 'M.', 'yurii@gmail.com', 'yurii@gmail.com', 'CLIENT');
+VALUES ('Yurii', 'M.', 'yurii@gmail.com', '$2a$10$i964LXhlsr0t.YHgiwX1ZeHV/YXjyQp.X3S93gIqgBLh.mwlfGaKS', 'CLIENT');
 
 INSERT INTO users (name, surname, email, password, role)
-VALUES ('Yuriy', 'D.', 'yuriy@gmail.com', 'yuriy@gmail.com', 'MASTER');
+VALUES ('Yuriy', 'D.', 'yuriy@gmail.com', '$2a$10$DqhoHPIwlZGG4DguQrPYGugFd7fo7efPuU.uD81LH7e8uZWm9B3tC', 'MASTER');
 
 INSERT INTO requests (client_id, description, viewed, accepted)
-VALUES (1, 'Change color of the car to red', 1, 0);
-
-INSERT INTO refusals (request_id, explanation, manager_id)
-VALUES (1, 'You can have a car painted any color that you want so long as it is black', 2);
-
-INSERT INTO feedback (request_id, text, score)
-VALUES (1, 'Did not accepted my request(', 1);
-
-INSERT INTO requests (client_id, description, viewed, accepted)
-VALUES (1, 'Change color of the car to black', 1, 1);
+VALUES (6, 'Replace the electrical plug of refrigerator', 1, 1);
 
 INSERT INTO orders (request_id, manager_id, price, master_id, repair_stage_id)
-VALUES (2, 2, 4500, 3, 8);
+VALUES (1, 14, 200, 1, 8);
 
+INSERT INTO feedback (request_id, text, score)
+VALUES (1, 'Very high quality and good service ', 10);
 
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (6, 'Replace the electrical plug of electric car', 1, 0);
+
+INSERT INTO refusals (request_id, explanation, manager_id)
+VALUES (2, 'We do not repair charging stations of electric car', 14);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (6, 'Replace the electrical plug of charger for remote control toy car', 1, 1);
+
+INSERT INTO orders (request_id, manager_id, price, master_id, repair_stage_id)
+VALUES (3, 14, 200, 4, 8);
+
+INSERT INTO feedback (request_id, text, score)
+VALUES (3, 'Very high quality and good service ', 10);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (15, 'Replace the electrical plug of washing machine', 1, 1);
+
+INSERT INTO orders (request_id, manager_id, price, master_id, repair_stage_id)
+VALUES (4, 12, 200, 1, 8);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (6, 'Replace the electrical plug of washing machine', 1, 1);
+
+INSERT INTO orders (request_id, manager_id, price, master_id, repair_stage_id)
+VALUES (5, 14, 200, 10, 8);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (6, 'Replace the electrical plug of dishwasher', 1, 1);
+
+INSERT INTO orders (request_id, manager_id, price, master_id, repair_stage_id)
+VALUES (6, 12, 200, 1, 8);
+
+INSERT INTO feedback (request_id, text, score)
+VALUES (6, 'High quality and good service ', 9);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (6, 'Replace the electrical plug of microwave oven', 1, 1);
+
+INSERT INTO orders (request_id, manager_id, price, master_id, repair_stage_id)
+VALUES (7, 14, 200, 13, 8);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (15, 'Replace the electrical plug of fan', 1, 1);
+
+INSERT INTO orders (request_id, manager_id, price, master_id, repair_stage_id)
+VALUES (8, 12, 200, 1, 8);
+
+INSERT INTO feedback (request_id, text, score)
+VALUES (9, 'Very high quality and good service ', 10);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (6, 'Replace the electrical plug of electric heater', 1, 1);
+
+INSERT INTO orders (request_id, manager_id, price, master_id, repair_stage_id)
+VALUES (9, 14, 200, 1, 8);
+
+INSERT INTO feedback (request_id, text, score)
+VALUES (9, 'Stop working in one hour after repair', 1);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (6, 'Replace the electrical plug of coffeemaker', 1, 1);
+
+INSERT INTO orders (request_id, manager_id, price, master_id, repair_stage_id)
+VALUES (10, 14, 200, 13, 6);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (6, 'Replace the electrical plug of electric kettle', 1, 1);
+
+INSERT INTO orders (request_id, manager_id, price, master_id, repair_stage_id)
+VALUES (11, 14, 200, 1, 8);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (6, 'Replace the electrical plug of toaster', 1, 1);
+
+INSERT INTO orders (request_id, manager_id, price, master_id, repair_stage_id)
+VALUES (12, 14, 200, 22, 8);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (15, 'Replace the electrical plug of blender', 1, 1);
+
+INSERT INTO orders (request_id, manager_id, price, master_id, repair_stage_id)
+VALUES (13, 14, 200, 1, 8);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (15, 'Replace the electrical plug of waffle iron', 1, 1);
+
+INSERT INTO orders (request_id, manager_id, price, master_id, repair_stage_id)
+VALUES (14, 12, 200, 17, 8);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (6, 'Replace the electrical plug of food processor', 1, 1);
+
+INSERT INTO orders (request_id, manager_id, price, master_id, repair_stage_id)
+VALUES (15, 14, 200, 1, 8);
+
+INSERT INTO feedback (request_id, text, score)
+VALUES (15, 'Manager was rude on the phone', 6);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (15, 'Replace the electrical plug of rice cooker', 1, 1);
+
+INSERT INTO orders (request_id, manager_id, price, master_id, repair_stage_id)
+VALUES (16, 12, 200, 1, 5);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (15, 'Replace the electrical plug of sewing machine', 0, 0);
+
+INSERT INTO requests (client_id, description, viewed, accepted)
+VALUES (6, 'Replace the electrical plug of air conditioner', 0, 0);
